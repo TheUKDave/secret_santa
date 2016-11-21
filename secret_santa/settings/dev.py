9640d37269aa73f8c0ed170d0107e5573370c8f6
David Downes <me@theukdave.com>
2016-11-21 09:04:17 +0000

    Added unicorn to serve on heroku Corrected name of wigs in procfile Add in IP restriction middleware
    Middleware rename
    
    Updated ip_check
    
    Updated middleware for django 1.10 compatibility
    
    Tidied IP settings
    
    Inherit from django 1.10 mixin for middleware

--- secret_santa/settings/dev.py
+++ secret_santa/settings/dev.py
@@ -1,5 +1,18 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 ADMINS = (('David Downes', 'github@theukdave.com'),)
+
+MIDDLEWARE += [
+    'santa.middleware.IpRestrictionMiddleware',
+]
+
+RESTRICT_IPS = os.environ.get('RESTRICT_IPS', '').lower() in ['true', 'yes', '1']
+
+allowed_ips = os.environ.get('ALLOWED_IPS', '')
+allowed_ip_ranges = os.environ.get('ALLOWED_IP_RANGES', '')
+
+# ALLOWED_IPS/_IP_RANGES should be a comma-separated list of ip address or network ranges
+ALLOWED_IPS = allowed_ips.split(',') if allowed_ips != '' else []
+ALLOWED_IP_RANGES = allowed_ip_ranges.split(',') if allowed_ip_ranges != '' else []
