714510257b038a125b4a123c4f8daf4f0b77f735
David Downes <me@theukdave.com>
2016-11-21 17:45:00 +0000

    updated allowed hosts

--- secret_santa/settings/prod.py
+++ secret_santa/settings/prod.py
@@ -1,5 +1,11 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['*.making-a-list.com']
+
+ALLOWED_HOSTS = [
+    'making-a-list.com', 'www.making-a-list.com',
+    'hes-making-a-list.com', 'www.hes-making-a-list.com',
+    'the-north-pole.christmas', 'www.the-north-pole.christmas',
+]
+
 ADMINS = (('David Downes', 'github@theukdave.com'),)
